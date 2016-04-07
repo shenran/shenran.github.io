b76e3db3aba799e03a4eb38db38a3b7bf7c51c7c
沈燃 <shenran@foxmail.com>
2016-04-07 10:44:40 +0800

    new

--- script/tools.js
+++ script/tools.js
@@ -4,5 +4,14 @@
 function getQueryString(name) {
     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
     var r = window.location.search.substr(1).match(reg);
-    if (r != null) return unescape(r[2]); return null;
+    if (r != null)
+        return r[2];
+    return null;
+}
+
+function httpget(app,url,index) {
+    app.controller('customersCtrl', function($scope, $http) {
+
+        $http.get(url)
+            .success(function(response) {$scope[index]=response;});});
 }
